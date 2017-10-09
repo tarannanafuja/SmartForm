dd1abe9dd5709e1d0cfa77c53a85bdde4fa45068
tarannanafuja <nafujataranna@gmail.com>
2017-10-09 10:34:53 +0600

    Assignment 7

--- smartForm.js
+++ smartForm.js
@@ -1,3 +1,45 @@
 // Your jQuery goes here
 
-var userData = {};
+var userData = {
+  name:"",
+  email:"",
+  html:{likes:[], dislikes:[]},
+  css:{likes:[], dislikes:[]},
+  js:{likes:[], dislikes:[]},
+  strengths: {html:"", css:"", js:""},
+  currentQuestion:"welcome"
+};
+
+$("#startBtn").click(function(event){
+  $("#welcome").hide();
+  $("#q1").show();
+});
+
+$("#name").change(function(event){
+  console.log($("#name").val());
+
+});
+$("#email").change(function(event){
+  console.log($("#email").val());
+  if("#email").val() !="" && $("#name").val()
+  $("#q1Next").prop("disable", false);}
+});
+$("#q1Next").click(function(event){userData.name=$("#name").val();
+userData.email=$("#email").val();
+$("#q1").hide();
+$("#q2").show();
+});
+
+$("#htmlQ").click(function(event){$("#q2").hide();
+$("#q2a").show());
+
+$("#cssQ").click(function(event){$("#q2").hide();
+$("#q2b").show());
+
+$("#jsQ").click(function(event){$("#q2").hide();
+$("#q2c").show());
+
+$(".prevQ2").click(function(event){
+$("#q2").show();
+$(this).parent().hide();
+});
